11aa3ccf1532d68187f04a1eaab3d85bf7d3b724
GitauMbogo <gitaumbogo3@gmail.com>
2020-09-11 13:01:19 +0300

    prophecy update

--- Prophecy/scripts.js
+++ Prophecy/scripts.js
@@ -1,26 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const mobile_menu = document.querySelector(".nav ul");
-const menu_item = document.querySelectorAll(".nav ul li a");
-const header = document.querySelector(".head");
+var today = new Date();
+var year = today.getFullYear();
 
+function updateYear() {
+	var el = document.getElementById("year");
+	el.textContent = year;
 
-hamburger.addEventListener("click",() => {
-	hamburger.classList.toggle("active");
-	mobile_menu.classList.toggle("active");
-});
+}
 
-document.addEventListener("scroll", () => {
-	var scroll_position = window.scrollY;
-	if(scroll_position > 250){
-		header.style.backgroundColor = "rgb(222, 220, 220)";
-	}else{
-		header.style.backgroundColor = "transparent";
-	}
-});
-
-menu_item.forEach((item) => {
-	item.addEventListener("click", () => {
-		  hamburger.classList.toggle("active");
-	      mobile_menu.classList.toggle("active");
-		});
-});
+updateYear();
